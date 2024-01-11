fb18bc3e334922702643d2e946b2e9d968b0c83a
s92509171 <s92509171@gmail.com>
2024-01-11 21:18:09 +0800

    促銷活動內容

--- src/pages/promotion/children/Promotions.vue
+++ src/pages/promotion/children/Promotions.vue
@@ -1,48 +1,62 @@
 <script setup>
 import { ref } from 'vue';
-const promotionItem = ref(
+const promotionItems = ref(
     [
         {
             img: '/public/img/20240103093546115.jpg',
             title:'1.1 ~ 1.17 限量滿額折價券',
             date:'2024.01.01',
             Classification:'促銷活動',
-            id: '1'
+            id: '0',
+            text:'1月,限量滿額折價券,請先登入會員,才可領取折價券'
         },
         {
             img: '/public/img/20240103092445839.jpg',
             title:'2024 新年快樂！凡會員一月首購(不限金額)輸入vip99即送好禮 HR 伸縮扣一個！',
             date:'2024.01.01',
             Classification:'促銷活動',
-            id: '2'
+            id: '1',
+            text:'2024新年快樂,,會員一月首購(不限金額)即送首購好禮 HR 伸縮扣乙個！,活動內容：會員當月首購，不限結帳金額,結帳時在折扣碼欄位輸入「vip99」,即贈HR HPI-2701 伸縮扣一個！'
+
+
+            
+
         },
         {
             img: '/public/img/20240103092926353.jpg',
             title:'2024_1月，消費單筆滿額贈好禮，白金海豚以上會員另有加碼贈！',
             date:'2024.01.01',
             Classification:'促銷活動',
-            id: '3'
+            id: '2',
+            text:'2024 Happy New Year 1月,,會員消費單筆滿額,送超實用禮物,白金海豚等級以上會員再加碼'
+
         },
         {
             img: '/public/img/20240102103040678.jpg',
             title:'2024 一月 單筆結帳滿額贈百元折價券',
             date:'2024.01.01',
             Classification:'促銷活動',
-            id: '4'
+            id: '3',
+            text:'活動時間：2024/01/01~01/31,,單筆結帳滿NT$1000元,隨貨附贈100元折價券'
+
         },
         {
             img: '/public/img/20231221134325112.jpg',
             title:'進口名牌釣具特價中 ~ 優惠7折起，數量有限售完為止！',
             date:'2024.12.21',
             Classification:'促銷活動',
-            id: '5'
+            id: '4',
+            text:'進口名牌釣具特價中❗,超多商品優惠7折起,數量有限,售完為止,一慢就沒啦~'
+
         },
         {
             img: '/public/img/20231215103929637.jpg',
             title:'HR 年終慶 品牌釣竿 4折起！',
             date:'2023.15.15',
             Classification:'促銷活動',
-            id: '6'
+            id: '5',
+            text:'Hearty Rise 品牌年終慶,超多款式釣竿,特價4折起'
+
         },
     ]
 )
@@ -51,14 +65,21 @@ const promotionItem = ref(
 </script>
 
 <template>
-    <div class="main-continer" >
-        <div class="card-container" v-for="promotionItems of promotionItem" :key="promotionItems.id">
-            <img :src="promotionItems.img" :alt="promotionItems.title">
+    <RouterView  :promotionItems="promotionItems"/>
+
+    <div class="main-continer" v-show="$route.path === '/promotion/Promotions'">
+        <div class="card-container" v-for="promotionItem of promotionItems" :key="promotionItem.id">
+
+            <RouterLink :to="{ name: 'PromotionsMessageDetails',query: { id: promotionItem.id }}">
+            <img :src="promotionItem.img" :alt="promotionItem.title">
+            </RouterLink>
+
+
             <div class="card-text">
-                <p>{{promotionItems.Classification}}</p>
-                <span>{{ promotionItems.date }}</span>
+                <p>{{promotionItem.Classification}}</p>
+                <span>{{ promotionItem.date }}</span>
             </div>
-            <h3>{{ promotionItems.title }}</h3>
+            <h3>{{ promotionItem.title }}</h3>
         </div>
     </div>
 </template>
@@ -105,5 +126,6 @@ const promotionItem = ref(
         font-size: 18px;
     }
 }
+
 
 </style>
--- src/pages/promotion/children/grandChildren/messageDetails.vue
+++ src/pages/promotion/children/grandChildren/messageDetails.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref  } from 'vue'
+import { computed, ref  } from 'vue'
 import { useRoute  } from 'vue-router'
 
 const props = defineProps(['promotionItems'])
@@ -7,13 +7,62 @@ const props = defineProps(['promotionItems'])
 const route = useRoute()
 const messageId = route.query.id
 const itemDetails = props.promotionItems[messageId]
-console.log(messageId);
 
-
+const formattedText = computed(() =>{
+    return itemDetails.text.replace(/,/g, '<br>');
+})
 </script>
 
 <template>
-
+<div class="wrap">
     <h3>{{ itemDetails.title }}</h3>
-    <div @click="updateProps">1231321321</div>
+    <div class="container">
+        <p class="Classification">{{ itemDetails.Classification }}</p>
+        <p class="date">{{ itemDetails.date }}</p>
+    </div>
+    <img :src="itemDetails.img" alt="">
+    <p class="text" v-html="formattedText"></p>
+</div>
 </template>
+<style lang="scss" scoped>
+.wrap{
+    max-width: 1270px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 30px;
+    .container{
+        display: flex;
+        margin: 30px 0;
+        text-align: center;
+        gap: 30px;
+        .Classification{
+            background-color: #6d6d6d;
+            color: #fff;
+            padding: 5px 10px;
+            border: 1px solid;
+            border-radius: 10px;
+        }
+        .date{
+            font-size: 18px;
+            line-height:30px ;
+        }
+    }
+    img{
+        max-width: 100%;
+        width: 500px;
+    }
+    h3{
+        color: #374e6d;
+        font-size: 25px;
+        font-weight: 600;
+    }
+    .text{
+        font-size: 18px;
+        margin: 30px 0;
+        text-align: start;
+        line-height: 30px;
+    }
+}
+
+</style>
